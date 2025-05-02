aef385d2e401d10cafdf329aaaa832961b6d31dc
catp757 <158765774+catp757@users.noreply.github.com>
2025-05-02 15:40:43 -0400

    Initial commit

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+def summation(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtraction(a, b):
+    """Subtract two numbers."""
+    return a - b
+
+def multiplication(a, b):
+    """Multiply two numbers."""
+    return a * b
